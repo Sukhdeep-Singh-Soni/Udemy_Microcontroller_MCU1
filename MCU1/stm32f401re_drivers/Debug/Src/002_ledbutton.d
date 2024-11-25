21269bc20eaedb749413267d5c526a3296f81bfc
Sukhdeep-Singh-Soni <sukhdeepsinghsoni1998@gmail.com>
2024-11-25 23:42:16 +0530

    section 27

---
+++ MCU1/stm32f401re_drivers/Debug/Src/002_ledbutton.d
@@ -0,0 +1,5 @@
+Src/002_ledbutton.o: ../Src/002_ledbutton.c \
+ /home/deep/Desktop/Udemy_Microcontroller_MCU1/MCU1/stm32f401re_drivers/drivers/inc/stm32f401re_gpio_driver.h \
+ /home/deep/Desktop/Udemy_Microcontroller_MCU1/MCU1/stm32f401re_drivers/drivers/inc/stm32f401re.h
+/home/deep/Desktop/Udemy_Microcontroller_MCU1/MCU1/stm32f401re_drivers/drivers/inc/stm32f401re_gpio_driver.h:
+/home/deep/Desktop/Udemy_Microcontroller_MCU1/MCU1/stm32f401re_drivers/drivers/inc/stm32f401re.h:
--- MCU1/stm32f401re_drivers/Debug/Src/main.d
+++
@@ -1,3 +0,0 @@
-Src/main.o: ../Src/main.c \
- /home/deep/Desktop/Udemy_Microcontroller_MCU1/MCU1/stm32f401re_drivers/drivers/inc/stm32f401re.h
-/home/deep/Desktop/Udemy_Microcontroller_MCU1/MCU1/stm32f401re_drivers/drivers/inc/stm32f401re.h:
