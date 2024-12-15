a159be86c83c873a282ebc97f6a1ddabd612aa1c
Sukhdeep-Singh-Soni <sukhdeepsinghsoni1998@gmail.com>
2024-12-15 12:08:17 +0530

    section 63

--- MCU1/stm32f401re_drivers/drivers/inc/stm32f401re_i2c_driver.h
+++ MCU1/stm32f401re_drivers/drivers/inc/stm32f401re_i2c_driver.h
@@ -9,6 +9,7 @@
 #define INC_STM32F401RE_I2C_DRIVER_H_
 
 #include "stm32f401re.h"
+#include <stddef.h>
 
 /*
  * Configuration structure for I2Cx peripheral
@@ -62,6 +63,11 @@ typedef struct {
 #define I2C_STATE_READY			0
 #define I2C_STATE_BUSY_IN_TX	1
 #define I2C_STATE_BUSY_IN_RX	2
+#define I2C_ERROR_BERR  		3
+#define I2C_ERROR_ARLO  		4
+#define I2C_ERROR_AF    		5
+#define I2C_ERROR_OVR   		6
+#define I2C_ERROR_TIMEOUT 		7
 
 /*
  * I2C SR1 and SR2 register masking bit definitions
@@ -90,6 +96,13 @@ typedef struct {
 #define I2C_DISABLE_SR		SET
 
 /*
+ * I2C Application events macros
+ * */
+#define I2C_EV_TX_CMPLT		0
+#define I2C_EV_RX_CMPLT		1
+#define I2C_EV_STOP			2
+
+/*
  * ##################### API's provided by this driver ###############################
  * */
 
@@ -111,11 +124,14 @@ void I2C_MasterSendData(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t l
 void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t len, uint8_t SlaveAddr, uint8_t Sr);
 uint8_t  I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pTxBuffer, uint32_t Len,uint8_t SlaveAddr,uint8_t Sr);
 uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
+
 /*
  * IRQ Handling and Interrupt config
  * */
 void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
 void I2C_IRQPRiorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+void I2C_EV_IRQHandling(I2C_Handle_t *pI2CHandle);
+void I2C_ER_IRQHandling(I2C_Handle_t *pI2CHandle);
 
 /*
  * Some other miscellaneous I2C api's
@@ -124,10 +140,15 @@ uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pSPIx, uint8_t FlagName);
 void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
 void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle, uint8_t I2CEvent); /*I2CEvent from @I2CEvent*/
 void I2C_GenerateSTOPCondition(I2C_RegDef_t *pI2Cx);
-void I2C_ClearADDRFlag(I2C_RegDef_t *pI2Cx);
+void I2C_ClearADDRFlag(I2C_Handle_t *pI2CHandle);
 void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
 static void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx);
 void I2C_ExecuteAddressPhaseRead(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
 void I2C_ManageAcking(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
+void I2C_MasterHandleTxEInterrupt(I2C_Handle_t *pI2CHandle);
+void I2C_MasterHandleRxNEInterrupt(I2C_Handle_t *pI2CHandle);
+void I2C_CloseReceiveData(I2C_Handle_t *pI2CHandle);
+void I2C_CloseSendData(I2C_Handle_t *pI2CHandle);
+void I2C_ER_IRQHandling(I2C_Handle_t *pI2CHandle);
 
 #endif /* INC_STM32F401RE_I2C_DRIVER_H_ */
