4c042047cf44a25a65601104b4d4e24a65523524
Sukhdeep-Singh-Soni <sukhdeepsinghsoni1998@gmail.com>
2024-12-15 13:03:29 +0530

    section 65

--- MCU1/stm32f401re_drivers/drivers/inc/stm32f401re_i2c_driver.h
+++ MCU1/stm32f401re_drivers/drivers/inc/stm32f401re_i2c_driver.h
@@ -63,11 +63,6 @@ typedef struct {
 #define I2C_STATE_READY			0
 #define I2C_STATE_BUSY_IN_TX	1
 #define I2C_STATE_BUSY_IN_RX	2
-#define I2C_ERROR_BERR  		3
-#define I2C_ERROR_ARLO  		4
-#define I2C_ERROR_AF    		5
-#define I2C_ERROR_OVR   		6
-#define I2C_ERROR_TIMEOUT 		7
 
 /*
  * I2C SR1 and SR2 register masking bit definitions
@@ -98,9 +93,16 @@ typedef struct {
 /*
  * I2C Application events macros
  * */
-#define I2C_EV_TX_CMPLT		0
-#define I2C_EV_RX_CMPLT		1
-#define I2C_EV_STOP			2
+#define I2C_EV_TX_CMPLT			0
+#define I2C_EV_RX_CMPLT			1
+#define I2C_EV_STOP				2
+#define I2C_ERROR_BERR  		3
+#define I2C_ERROR_ARLO  		4
+#define I2C_ERROR_AF    		5
+#define I2C_ERROR_OVR   		6
+#define I2C_ERROR_TIMEOUT 		7
+#define I2C_EV_DATA_REQ			8
+#define I2C_EV_DATA_RECV		9
 
 /*
  * ##################### API's provided by this driver ###############################
@@ -124,6 +126,8 @@ void I2C_MasterSendData(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t l
 void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t len, uint8_t SlaveAddr, uint8_t Sr);
 uint8_t  I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pTxBuffer, uint32_t Len,uint8_t SlaveAddr,uint8_t Sr);
 uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
+void I2C_SlaveSendData(I2C_RegDef_t *pI2Cx, uint8_t data);
+uint8_t I2C_SlaveReceiveData(I2C_RegDef_t *pI2Cx);
 
 /*
  * IRQ Handling and Interrupt config
